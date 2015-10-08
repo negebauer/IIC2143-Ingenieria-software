b6884aa3673d5159b31b4fc50b25d1fc606b17ec
dacasas <dacasas@uc.cl>
2015-10-08 17:17:25 -0300

    actualice Assistant y Professor

--- AcademicManager/Backend/System/Users/Assistant.java
+++ AcademicManager/Backend/System/Users/Assistant.java
@@ -7,8 +7,6 @@ import Tools.Enums.Gender;
 public class Assistant extends User {
 
 	public Assistant(String name, String lastname, String rut, Gender gender, int age) {
-		super(name, lastname, rut, Access.Assistant);
-		this.setGender(gender);
-		this.setAge(age);
+		super(name, lastname, rut, Access.ReadOnly);
 	}
 }
--- AcademicManager/Backend/System/Users/Professor.java
+++ AcademicManager/Backend/System/Users/Professor.java
@@ -5,10 +5,8 @@ import Tools.Enums.Access;
 import Tools.Enums.Gender;
 
 public class Professor extends User {
-
+	
 	public Professor(String name, String lastname, String rut, Gender gender, int age) {
-		super(name, lastname, rut, Access.Professor);
-		this.setAge(age);
-		this.setGender(gender);
+		super(name, lastname, rut, Access.Editor);
 	}
 }
