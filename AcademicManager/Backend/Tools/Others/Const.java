2aa23d84ce5e50d2a19aa04117753e736c1395bc
nicogeb <negebauer@uc.cl>
2015-10-12 20:47:04 -0300

    Some syntax corrected. User relate updated

--- AcademicManager/Backend/Tools/Others/Const.java
+++ AcademicManager/Backend/Tools/Others/Const.java
@@ -7,6 +7,8 @@ public class Const {
 	public final static int DEFAULT_CREDITS = 10;
 	
 	public final static int DEFAULT_CREDITS_PER_SEMESTER = 55;
+	
+	public final static int DEFAULT_MAX_FAILED_CREDITS = 50;
 		
 	public final static String JAXP_SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
 
