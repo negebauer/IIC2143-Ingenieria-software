f906748d256abfa09fa4ccea4cc29c53fbde0787
dacasas <dacasas@uc.cl>
2015-10-21 22:51:21 -0300

    [Frontend] Testing controllers connection, and button actions

--- AcademicManager/src/TestFolder/HomeController.java
+++ AcademicManager/src/TestFolder/HomeController.java
@@ -1,10 +1,15 @@
 package TestFolder;
 
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+
 public class HomeController {
 	
+	private Button btnCreateNewSemester;
 	
 	public void btnCreateNewSemester_Pressed(){
-		
+		System.out.println("hola si funciona");
 	}
 	
 }
--- AcademicManager/src/TestFolder/TestClass.java
+++ AcademicManager/src/TestFolder/TestClass.java
@@ -1,49 +1,40 @@
 package TestFolder;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
 
 import System.Courses.Schedule;
 import Tools.Others.Messages;
 import Tools.Others.Utilities;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import Tools.Others.Messages.Message;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
 
-public class TestClass {
+public class TestClass extends Application{
 
 	public static void main(String [ ] args) {
-		System.out.println("Hello, World");
-		
-//		TODO Delete this class
-		
-		Schedule.Day day = Schedule.Day.MONDAY;
-		System.out.println(day.getDayString());
-		
-		Date date;
-		date = Utilities.getDateFromString("10.09.1993 18:58");
-		System.out.println(Utilities.getStringFromDate(date));
-	
-		System.out.println(Messages.getMessage(Message.COURSE_WASNT_ADDED_TO_SEMESTER_REPEATED.index()));
-		
-		System.out.println(Utilities.getDateFromString("10.09.1993" + " 00:00"));
-		
-		String response = "\n\nHola\nque\ntal\n\nhay dos lineas antes?\n\n\n3?\nfunciono?";
-		response = Utilities.cleanNewLineCharExcessFromString(response);
-		System.out.println(response);
-		for (String s : response.split("\n")) {
-			System.out.println(s + "awdawdawdaw");
-		}
-		response = String.join("\n", response.split("\n"));
-		
-		public void initTestView() {
-			try {
-				FXMLLoader loader = new FXMLLoader();
-								
-			} catch {
-				
-			} finally {
-				
-			}
-		}
-		System.out.println(response);
+			Application.launch(TestClass.class, args);
 	}
+
+	@Override
+	public void start(Stage primaryStage){
+		  URL location = getClass().getResource("Home.fxml");
+		  FXMLLoader fxmlLoader = new FXMLLoader(location);
+		  fxmlLoader.setController(new HomeController());
+		  Pane root = null;
+		  try {
+		    root = (Pane)fxmlLoader.load();
+		  } catch (IOException e) {
+		    e.printStackTrace();
+		  }
+		  
+		  Scene scene = new Scene(root,800,600);
+		  primaryStage.setScene(scene);
+		  primaryStage.show();
+		}
 } 
