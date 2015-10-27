61e8ad35ec966b1e1a838a98eb7cd0d504303e03
dacasas <dacasas@uc.cl>
2015-10-27 01:39:36 -0300

    Users in choicebox on login

--- AcademicManager/src/frontend/view/main/LogInController.java
+++ AcademicManager/src/frontend/view/main/LogInController.java
@@ -1,11 +1,15 @@
 package frontend.view.main;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import backend.manager.Manager;
 import backend.others.Const;
 import backend.others.Messages;
 import backend.others.Messages.UILabel;
+import backend.users.*;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -33,6 +37,28 @@ public class LogInController implements Initializable, IController {
 		labelSignIn.setText(Messages.getUILabel(UILabel.SIGN_IN_AS_USER));
 		labelRegistration.setText(Messages.getUILabel(UILabel.DONT_HAVE_ACCOUNT_REGISTER));
 		labelLogIn.setText(Messages.getUILabel(UILabel.LOG_IN));
+		
+		ArrayList<String> users = new ArrayList<String>();
+		
+		for (Professor professor : Manager.INSTANCE.professors) {
+			users.add(professor.getName() + " " + professor.getLastnameFather() + " " + professor.getLastnameMother());
+		}
+		
+		for (Student student : Manager.INSTANCE.students) {
+			users.add(student.getName() + " " + student.getLastnameFather() + " " + student.getLastnameMother());
+		}
+		
+		for (Assistant assistant : Manager.INSTANCE.assistants) {
+			users.add(assistant.getName() + " " + assistant.getLastnameFather() + " " + assistant.getLastnameMother());
+		}
+		
+		for (Admin admin : Manager.INSTANCE.admins) {
+			users.add(admin.getName() + " " + admin.getLastnameFather() + " " + admin.getLastnameMother());
+		}
+		
+		chBxUsers.setItems(FXCollections.observableArrayList(users));
+		
+		
 	}
 	
 	public void btnSignIn_Pressed(){
