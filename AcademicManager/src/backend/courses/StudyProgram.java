3051267cd9900f8fd2ea34c82ea5242a023e7f80
nicogeb <negebauer@uc.cl>
2015-11-05 17:42:34 -0300

    Added addSemester to StudyProgram

--- AcademicManager/src/backend/courses/StudyProgram.java
+++ AcademicManager/src/backend/courses/StudyProgram.java
@@ -79,6 +79,10 @@ public class StudyProgram {
 		this.semesters = semesters;
 	}
 	
+	public void addSemester(Semester semester) {
+		this.semesters.add(semester);
+	}
+	
 	/** 
 	 * @return The maximum of credits that one semester can have.
 	 */
