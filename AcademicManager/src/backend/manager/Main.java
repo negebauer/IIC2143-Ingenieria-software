a210b2a2d936223634ef41e26e75f5e1aaf0a6f0
nicogeb <negebauer@uc.cl>
2015-11-08 14:51:01 -0300

    Deleted an import

--- AcademicManager/src/backend/manager/Main.java
+++ AcademicManager/src/backend/manager/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import frontend.main.MLogInController;
 import frontend.others.CurrentViewHandler;
-import backend.others.Range;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
