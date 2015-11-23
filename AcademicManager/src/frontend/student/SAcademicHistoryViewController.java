447759d654e075cf3334b94ccce9e753be7603d7
mgtapia <mgtapia@uc.cl>
2015-11-23 16:19:59 -0300

    cambios en academic history

--- AcademicManager/src/frontend/student/SAcademicHistoryViewController.java
+++ AcademicManager/src/frontend/student/SAcademicHistoryViewController.java
@@ -3,14 +3,13 @@ package frontend.student;
 import java.net.URL;
 import java.util.ArrayList;
 
-import backend.courses.Course;
 import backend.courses.Coursed;
 import backend.courses.CoursedSemester;
-import backend.courses.Semester;
 import backend.courses.StudyProgram;
 import backend.manager.Manager;
 import backend.users.Student;
 import frontend.main.MViewController;
+import frontend.others.Validate;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -24,6 +23,7 @@ public class SAcademicHistoryViewController extends MViewController {
 	TextArea txAAcademic;
 	@FXML
 	ComboBox<String> chBxCarreer;
+	
 	Student user = (Student) Manager.INSTANCE.currentUser;
 	static URL view = Object.class.getResource("/frontend/student/SAcademicHistoryView.fxml");
 	String carreer = "";
@@ -35,7 +35,6 @@ public class SAcademicHistoryViewController extends MViewController {
 		ArrayList<String> sp = new ArrayList<String>();
 		for (StudyProgram p : user.getCurriculum().getStudyPrograms()) {
 			sp.add(p.getName());
-			System.out.println(p.getName());
 		}
 		chBxCarreer.setItems(FXCollections.observableArrayList(sp));
 		chBxCarreer.setOnAction((event) -> {
@@ -46,14 +45,14 @@ public class SAcademicHistoryViewController extends MViewController {
 	}
 	
 	public void refresh() {
-		String fullText = "";
-		String academic = "";
+		String fullText = "Promedio General: ";
+		String academic = "Promedio Carrera: ";
 		double totalGrade = 0;
 		double totalCarreerGrade = 0;
 		int totalCourses = 0;
 		for (Coursed coursed : user.getCurriculum().getCoursedCourses()) {
 			totalGrade += coursed.getGrade();		
-			if (checkCourse(coursed.getInitials())) {
+			if (Validate.checkCourse(coursed.getInitials(), carreer)) {
 				totalCarreerGrade += coursed.getGrade();
 				totalCourses++;
 			}
@@ -66,36 +65,32 @@ public class SAcademicHistoryViewController extends MViewController {
 		academic += totalCarreerGrade + "\n\n";
 		for (CoursedSemester coursedSemester : user.getCurriculum().getCoursedSemesters()) {
 			fullText += coursedSemester.getYear() + "-" + coursedSemester.getSemester().getSemesterNumber() + ": "
-					+ coursedSemester.getGrade() + "\n\t";
-			academic += coursedSemester.getYear() + "-" + coursedSemester.getSemester().getSemesterNumber() + ": "
-					+ coursedSemester.getGrade() + "\n\t";
+					+ coursedSemester.getGrade() + "\n\t";			
+			String aux = "";
+			double sum = 0;
+			double prom = 0;
+			int count = 0;
 			for (Coursed coursed : coursedSemester.getCoursedCourses()) {
 				fullText += coursed.getInitials() + "-" + coursed.getSection() + " " + coursed.getName() + ": "
 						+ coursed.getGrade() + "\n\t";
-				if (checkCourse(coursed.getInitials())) {
-					academic += coursed.getInitials() + "-" + coursed.getSection() + " " + coursed.getName() + ": "
+				if (Validate.checkCourse(coursed.getInitials(), carreer)) {
+					aux += coursed.getInitials() + "-" + coursed.getSection() + " " + coursed.getName() + ": "
 							+ coursed.getGrade() + "\n\t";
+					sum += coursed.getGrade();
+					count++;
 				}
+			}			
+			if (count > 0) {
+				prom = sum/count;
 			}
+			else {
+				aux += "No hay ramos este semestre\n";
+			}
+			academic += coursedSemester.getYear() + "-" + coursedSemester.getSemester().getSemesterNumber() + ": "
+					+ prom + "\n\t" + aux + "\n";
 			fullText += "\n";
-			academic +="\n"; 
 		}
 		txAAcademicHistory.setText(fullText);
 		txAAcademic.setText(academic);
-	}
-	
-	private boolean checkCourse(String initials) {
-		for (StudyProgram sp : Manager.INSTANCE.studyPrograms) {
-			if (sp.getName().equals(carreer)) {
-				for (Semester s : sp.getSemesters()) {
-					for (Course c : s.getCourses()) {
-						if (c.getInitials().equals(initials)) {
-							return true;
-						}
-					}
-				}
-			}
-		}
-		return false;
 	}
 }
